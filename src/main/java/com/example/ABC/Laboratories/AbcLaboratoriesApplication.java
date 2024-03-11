7557c105b36dec6948c1d8fc883c627dc81d7138
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-11 12:26:20 +0530

    Initial Commit

---
+++ src/main/java/com/example/ABC/Laboratories/AbcLaboratoriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ABC.Laboratories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcLaboratoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcLaboratoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ABC/Laboratories/AbcLaboratoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ABC.Laboratories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcLaboratoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
