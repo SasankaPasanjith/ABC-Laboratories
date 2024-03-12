9b0ff2debe4691a7e371cb4eb2568bb525d0e0fb
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-12 13:39:41 +0530

    Create contact repo

---
+++ src/main/java/com/example/ABC/Laboratories/Repository/contactRepo.java
@@ -0,0 +1,9 @@
+package com.example.ABC.Laboratories.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ABC.Laboratories.Model.contact;
+
+public interface contactRepo extends JpaRepository<contact, Long>{
+
+}
