740fb4a42861bc12d0338e7494b1cbe8609d19ab
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-13 17:28:30 +0530

    Added some features to doctor controller

--- src/main/java/com/example/ABC/Laboratories/Controller/doctorController.java
+++ src/main/java/com/example/ABC/Laboratories/Controller/doctorController.java
@@ -1,12 +1,55 @@
 package com.example.ABC.Laboratories.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ABC.Laboratories.Exception.ResourceNotFoundException;
+import com.example.ABC.Laboratories.Model.doctor;
+import com.example.ABC.Laboratories.Repository.doctorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/consult/")
 public class doctorController {
 
+    @Autowired
+    private doctorRepo consultRepository;
+
+    // create doctor rest api
+    @PostMapping("/saveconsult")
+    public doctor createUsers(@RequestBody doctor consult) {
+        return consultRepository.save(consult);
+    }
+
+    // get all doctors rest api
+    @GetMapping("/getconsult")
+    private List<doctor> getAllConsult() {
+        return consultRepository.findAll();
+    }
+
+    // get doctor by Id rest api
+    @GetMapping("/getconsult/{id}")
+    public ResponseEntity<doctor> getconsultById(@PathVariable Long id) {
+        doctor consult = consultRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Doctors not exist with id : " + id));
+        return ResponseEntity.ok(consult);
+    }
+
+    // update doctor rest api
+    @PutMapping("/getconsult/{id}")
+    public ResponseEntity<doctor> updateConsult(@PathVariable Long id, @RequestBody doctor consult) {
+        doctor consult2 = consultRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Doctors not exist with id : " + id));
+        consult2.setName(consult.getName());
+        consult2.setEmail(consult.getEmail());
+        consult2.setStart_time(consult.getStart_time());
+        consult2.setEnd_time(consult.getEnd_time());
+        consult2.setNic(consult.getNic());
+        consult2.setPassword(consult.getPassword());
+
+        doctor updateConsult = consultRepository.save(consult2);
+        return ResponseEntity.ok(updateConsult);
+    }
 }
