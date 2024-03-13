ab899849d92058cfc19e4689340893e5a21ef353
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-13 17:52:39 +0530

    create contactController

---
+++ src/main/java/com/example/ABC/Laboratories/Controller/contactController.java
@@ -0,0 +1,10 @@
+package com.example.ABC.Laboratories.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class contactController {
+}
