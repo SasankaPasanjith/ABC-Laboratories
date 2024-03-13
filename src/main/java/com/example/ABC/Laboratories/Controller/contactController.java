75096be1b814be51ba472035c50025cad4797e7a
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-13 18:02:32 +0530

    added some features to contactController

--- src/main/java/com/example/ABC/Laboratories/Controller/contactController.java
+++ src/main/java/com/example/ABC/Laboratories/Controller/contactController.java
@@ -1,10 +1,31 @@
 package com.example.ABC.Laboratories.Controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ABC.Laboratories.Model.contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import com.example.ABC.Laboratories.Model.contact;
+import com.example.ABC.Laboratories.Repository.contactRepo;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api/v1/contact/")
 public class contactController {
+    @Autowired
+    private contactRepo contactRepo;
+
+    // create users rest api
+    @PostMapping("/savecontact")
+    public contact createContact(@RequestBody contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    // get all Users rest api
+    @GetMapping("/getContact")
+    private List<contact> getAllContact() {
+        return contactRepo.findAll();
+    }
 }
