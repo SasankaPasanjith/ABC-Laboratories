c494507a21223c603173500581376dc255d19c6c
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-13 17:02:06 +0530

    Added some features to user controller

--- src/main/java/com/example/ABC/Laboratories/Controller/usersController.java
+++ src/main/java/com/example/ABC/Laboratories/Controller/usersController.java
@@ -1,13 +1,66 @@
 package com.example.ABC.Laboratories.Controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ABC.Laboratories.Exception.ResourceNotFoundException;
+import com.example.ABC.Laboratories.Model.users;
+import com.example.ABC.Laboratories.Repository.usersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/users/")
 public class usersController {
+    @Autowired
+    private usersRepo usersRepository;
+
+    // create users rest api
+    @PostMapping("/saveusers")
+    public users createUsers(@RequestBody users users) {
+        return usersRepository.save(users);
+    }
+    // get all Users rest api
+    @GetMapping("/getusers")
+    private List<users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+    // get User by Id rest api
+    @GetMapping("/getusers/{id}")
+    public ResponseEntity<users> getUsersById(@PathVariable Long id) {
+        users users = usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id : " + id));
+        return ResponseEntity.ok(users);
+    }
+
+    // update User rest api
+    @PutMapping("/getusers/{id}")
+    public ResponseEntity<users> updateUsers(@PathVariable Long id, @RequestBody users dusers) {
+        users users = usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id : " + id));
+        users.setName(dusers.getName());
+        users.setAddress(dusers.getAddress());
+        users.setMobile(dusers.getMobile());
+        users.setPassword(dusers.getPassword());
+
+        users updateUsers = usersRepository.save(users);
+        return ResponseEntity.ok(updateUsers);
+    }
+
+    // delete User rest api
+    @DeleteMapping("/getusers/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
+        users users = usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id : " + id));
+
+        usersRepository.delete(users);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 
 }
