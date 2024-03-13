1f8bee14b9b70d8c049ccd1e93a395ce33e2d482
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-03-13 16:38:53 +0530

    Create usersController

---
+++ src/main/java/com/example/ABC/Laboratories/Controller/usersController.java
@@ -0,0 +1,13 @@
+package com.example.ABC.Laboratories.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/users/")
+public class usersController {
+
+}
